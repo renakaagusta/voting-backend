15233a1ece13e502640d6721d6822280a0db2528
renakaagusta <renakaagusta28@gmail.com>
2020-11-30 16:34:19 +0700

    de

--- controller/mainController.js
+++ controller/mainController.js
@@ -32,8 +32,10 @@ exports.login = function (req, res) {
 
 // Handle announcement actions
 exports.announcement = function (req, res) {
-
-  console.log("ip: "+JSON.stringify(ip));
+    if(!ip.includes(req.ip.replace(('"::ffff:', '')))){
+        console.log(req.ip.replace(('"::ffff:', '')));
+        return res.status(500).send();
+    }
     Setting.find({}, function(err, announcement) {
         if(err)
             throw err;
